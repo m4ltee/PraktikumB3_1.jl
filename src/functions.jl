57600f2b0f74e942032b1b755d860c84dce898a3
malte <malte-puetz@web.de>
2020-12-17 17:59:10 +0100

    added documentation

--- src/functions.jl
+++ src/functions.jl
@@ -1,3 +1,19 @@
+"""
+Die Funktion divide_jl ist eine julia Implementierung von divide.py.
+Diese Funktion arbeitet nicht mit .txt Dateien sondern mit arrays.
+
+### Argumente
+- `data::Array`: enthält die Messzeiten (erster Parameter)
+- `Δt::Real`: Länge der Intervalle in s (zweiter Parameter)
+
+### Rückgabewert
+- `counts::Array`: enthält Anzahl der Signale für jedes Interval
+
+### Beispiel
+```julia
+counts = divide_jl(data, 10)
+```
+"""
 function divide_jl(data::Array, Δt::Real)
     counts = []
     for x in data
@@ -10,6 +26,26 @@ function divide_jl(data::Array, Δt::Real)
     pop!(counts)
     return counts
 end
+"""
+Die Funktion binomial_jl ist eine julia Implementierung von binomial.py.
+Diese Funktion arbeitet nicht mit .txt Dateien sondern mit arrays.
+
+### Argumente
+- `data::Array`: enthält die Messzeiten (erster Parameter)
+- `Δt::Real`: Länge der Intervalle in s (zweiter Parameter)
+- `n::Int`: Binningparameter (dritter Parameter)
+
+### Rückgabewert
+- `x::Array`: enthält die x-Werte der Binomialverteilung
+- `y::Array`: enthält die y-Werte der Binomialverteilung
+
+### Beispiel
+
+
+```julia
+x, y = binomial_jl(data, 0.2, 1)
+```
+"""
 function binomial_jl(data::Array, Δt::Real, n::Int)
     counts = divide_jl(data, Δt)
     hist1 = []
@@ -31,6 +67,26 @@ function binomial_jl(data::Array, Δt::Real, n::Int)
 
     return [i for i in 0:length(hist2)-1], hist2
 end
+"""
+Die Funktion interval_jl ist eine julia Implementierung von interval.py.
+Diese Funktion arbeitet nicht mit .txt Dateien sondern mit arrays.
+
+### Argumente
+- `data::Array`: enthält die Messzeiten (erster Parameter)
+- `Δt::Real`: Länge der Intervalle in s (zweiter Parameter)
+- `n::Int`: Binningparameter (dritter Parameter)
+
+### Rückgabewert
+- `x::Array`: enthält die x-Werte der Intervallverteilung
+- `y::Array`: enthält die y-Werte der Intervallverteilung
+
+### Beispiel
+
+
+```julia
+x, y = interval_jl(data, 0.001, 1)
+```
+"""
 function interval_jl(data::Array, Δt::Real, n::Int)
     hist = []
     for i in 1:length(data)-n
